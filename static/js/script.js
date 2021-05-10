6493ba2dd573350afea64e344f8adbfe04f23a26
João Pedro Brandão e Silva <38329386+jpbrs@users.noreply.github.com>
2021-05-10 18:18:22 -0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 async function checkPassword() {
     if (document.getElementById('password').value.toUpperCase() == 'SIC AGORINHA CREATUS EST') {
         await alert('Beatus homo qui invenit sapientiam et qui affluit prudentia');
-        location.href = "http://google.com";
+        location.href = "https://google.com";
         return true;
     } else {
         location.href = ('https://letmegooglethat.com/?q=como+hackear+o+agorinha');
